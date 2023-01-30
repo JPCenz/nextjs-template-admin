e83b2b74dc88a5804a5de0fb63eeee677a7d6ce2
JPCenz <p.cenzano5@gmail.com>
2023-01-30 15:16:13 -0500

    initial commit

---
+++ pages/login.tsx
@@ -0,0 +1,19 @@
+import Footer from "@/components/Footer"
+import Head from "next/head"
+
+const login = () => {
+    return(
+        <>
+            <Head>
+                <title>Login</title>
+            </Head>
+            <div>
+                <h1>HELLO LOGIN</h1>
+            </div>
+            <Footer></Footer>
+        </>
+    )
+
+}
+
+export default login

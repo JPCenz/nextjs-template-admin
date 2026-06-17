fb4c1d47b0f41b222a05fd3d65d13bd44be4cdee
JPCenz <p.cenzano5@gmail.com>
2026-06-16 23:39:40 -0500

    feat: add button component 2

--- src/components/component2.tsx
+++ src/components/component2.tsx
@@ -4,6 +4,7 @@ export function Component2() : any {
     <div>
       <h1>Component 2</h1>
       <p>This is component 2.</p>
+      <button onClick={() => alert('Component 2 button clicked!')}>Click Me</button>
 
     </div>
   );

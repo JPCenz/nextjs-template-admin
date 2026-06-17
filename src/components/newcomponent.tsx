2390d8a8cca31d47372cbd4b31e25d5badaf2dcb
JPCenz <p.cenzano5@gmail.com>
2026-06-16 22:59:39 -0500

    chore> add butom

--- src/components/newcomponent.tsx
+++ src/components/newcomponent.tsx
@@ -4,6 +4,7 @@ export function NewComponent() : JSX.Element {
     <div>
       <h1>New Component</h1>
       <p>This is a new component.</p>
+      <button onClick={() => alert('Button clicked!')}>Click Me</button>
     </div>
   );
 }
